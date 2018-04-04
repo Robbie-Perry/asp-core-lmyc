3888610357089f2d52bfc1ea8ebf99d724f7d745
Robbie Perry <robbieperry22@gmail.com>
2018-04-03 17:25:02 -0700

    Seed Admin and User roles, as well as test accounts. Closes #1

--- asp-core-lmyc/Data/SeedData.cs
+++ asp-core-lmyc/Data/SeedData.cs
@@ -1,5 +1,7 @@
 ﻿using asp_core_lmyc.Data;
+using asp_core_lmyc.Models;
 using LmycWeb.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -10,10 +12,10 @@ namespace LmycWeb.Data
 {
     public static class SeedData
     {
-        public static void Initialize(ApplicationDbContext context)
+        public static void SeedBoats(ApplicationDbContext context)
         {
             context.Database.EnsureCreated();
-            
+
             if (context.Boats.Any())
             {
                 return;
@@ -57,6 +59,74 @@ namespace LmycWeb.Data
                 context.Boats.Add(b);
             }
             context.SaveChanges();
+        }
+
+        public static void SeedUsers(UserManager<ApplicationUser> userManager)
+        {
+            IdentityResult result;
+            if (userManager.FindByEmailAsync("a@a.a").Result == null)
+            {
+                ApplicationUser admin = new ApplicationUser()
+                {
+                    Email = "a@a.a",
+                    UserName = "a@a.a",
+                    FirstName = "aFirst",
+                    LastName = "aLast",
+                    Street = "aStreet",
+                    City = "aCity",
+                    Province = "aProvince",
+                    PostalCode = "aPostalCode",
+                    Country = "aCountry",
+                    MobileNumber = "aMobile",
+                    SailingExperience = "aExperience"
+                };
+
+                result = userManager.CreateAsync(admin, "P@$$w0rd").Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(admin, "Admin").Wait();
+                }
+            }
+            if (userManager.FindByEmailAsync("m@m.m").Result == null)
+            {
+                ApplicationUser member = new ApplicationUser()
+                {
+                    Email = "m@m.m",
+                    UserName = "m@m.m",
+                    FirstName = "mFirst",
+                    LastName = "mLast",
+                    Street = "mStreet",
+                    City = "mCity",
+                    Province = "mProvince",
+                    PostalCode = "mPostalCode",
+                    Country = "mCountry",
+                    MobileNumber = "mMobile",
+                    SailingExperience = "mExperience"
+                };
+                result = userManager.CreateAsync(member, "P@$$w0rd").Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(member, "Member").Wait();
+                }
+            }
+        }
+
+        public static void SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if (!roleManager.RoleExistsAsync("Member").Result)
+            {
+                IdentityRole role = new IdentityRole();
+                role.Name = "Member";
+                IdentityResult roleResult = roleManager.CreateAsync(role).Result;
+            }
+            if (!roleManager.RoleExistsAsync("Admin").Result)
+            {
+                IdentityRole role = new IdentityRole();
+                role.Name = "Admin";
+                IdentityResult roleResult = roleManager.CreateAsync(role).Result;
+            }
         }
     }
 }
