93bac2df0dc6e9721c5f79e9ccfea819ed4514f5
HamishBrindle <hamishbrindle@gmail.com>
2018-04-04 19:29:22 -0700

    Added reservations

--- asp-core-lmyc/Data/Migrations/20180404222132_AddReservations.Designer.cs
+++ asp-core-lmyc/Data/Migrations/20180404222132_AddReservations.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace asp_core_lmyc.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180404222132_AddReservations")]
+    partial class AddReservations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
