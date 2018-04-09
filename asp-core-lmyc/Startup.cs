69ac6dbaa7a062bef8c78fd0009d437b31c88495
Robbie Perry <robbieperry22@gmail.com>
2018-04-08 23:47:53 -0700

    ugh

--- asp-core-lmyc/Startup.cs
+++ asp-core-lmyc/Startup.cs
@@ -108,8 +108,7 @@ namespace asp_core_lmyc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env,
             UserManager<ApplicationUser> userManager,
-            RoleManager<IdentityRole> roleManager,
-            ApplicationDbContext context)
+            RoleManager<IdentityRole> roleManager)
         {
             SeedData.SeedRoles(roleManager);
             SeedData.SeedUsers(userManager);
